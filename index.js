795583c34628c46ccb487a6e94d66e0953fe352b
Mildred Hart <mildredhart54@gmail.com>
2022-06-22 11:30:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var firstName ="Mildred";
+var middleName = "Benjamin"
+var lastName = "Hart";
+var height = "5'9";
+var country= "Nigeria";
+
+
+let name= firstName + " " + middleName + " " + lastName;
+
+document.write("Name:" + " " +name + "<br>");
+document.write("Height:" + " " +height + "<br>");
+document.write("Country:" + " " +country);
